f0200918edae7be5b2722f195cef6c3c6d487b22
thuytram2803 <dangthithuytram01@gmail.com>
2025-09-07 04:42:10 +0700

    ud

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(
     cors({
-        origin: "https://codebase-21c3.onrender.com/",
+        origin: "https://codebase-21c3.onrender.com",
         credentials: true,
     })
 )
